89705c21fc94608cacec4091525db24cd248602d
alexcohal <a.al.alex.al.a@gmail.com>
2024-10-20 11:24:11 +0200

    Adding files

---
+++ welcome_page.js
@@ -0,0 +1,19 @@
+function scrollToProjects() {
+  event.preventDefault();
+    const element = document.getElementById('below_welcome');
+    element.scrollIntoView({ behavior: 'smooth' });
+  }
+  function scrollToCapacities() {
+    event.preventDefault();
+    const element = document.getElementById('cap');
+    element.scrollIntoView({ behavior: 'smooth' });
+  }
+  function scrollToContact() {
+    event.preventDefault();
+    const element = document.getElementById('ch2');
+    element.scrollIntoView({ behavior: 'smooth' });
+  }
+
+
+
+  
